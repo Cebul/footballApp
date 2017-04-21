f1f05c2154f02d506417aff50782a196eb678e16
cebul <kamilcebula1@gmail.com>
2017-04-21 11:37:47 +0200

    first commit

---
+++ client/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Router, Route, browserHistory } from 'react-router';
+
+import App from '../imports/ui/App';
+import New from '../imports/ui/New';
+
+injectTapEventPlugin();
+
+Meteor.startup(() => {
+  render((
+    <Router history={browserHistory}>
+      <Route path="/" component={App}/>
+      <Route path="/new" component={New}/>
+      <Route path="*" component={App}/>
+    </Router>
+  ), document.getElementById('render-target'));
+});
